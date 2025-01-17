2098b39327622a39ad4b1ed707e3a9882072d90e
PAlagusurya <alagusuryap@gmail.com>
2025-01-17 10:33:54 +0530

    Note ADDED to reduce method

--- JS_Concepts/reduce/reduce.js
+++ JS_Concepts/reduce/reduce.js
@@ -39,7 +39,7 @@ const { totalCartItems, totalPriceOfTheCart } = cart.reduce(
 console.log(totalCartItems, Number(totalPriceOfTheCart.toFixed(2)));
 
 /* What is the initialValue if not specified explicitly? */
-// const objects = [{ x: 1 }, { x: 2 }, { x: 3 }];
+const objects = [{ x: 1 }, { x: 2 }, { x: 3 }];
 // const sum = objects.reduce(
 //   (accumulator, currentValue) => accumulator + currentValue.x
 // );
@@ -64,3 +64,11 @@ const fetchData = async () => {
 
 const finalList = await fetchData();
 console.log(finalList);
+
+/**
+ * NOTE: The operation accumulator + currentValue.x tries to add an object ({ x: 1 }) and a number (2). When you attempt this, the object gets coerced into a string (via toString), which results in [object Object]2.
+ * When an object is coerced, it means that JavaScript automatically converts the object into a primitive value (like a string or a number) because of the context in which it's used. This process happens behind the scenes whenever JavaScript encounters
+ * an object in a situation where a primitive value is required
+ *
+ *
+ */
