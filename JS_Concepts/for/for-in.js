350f224cf99b49f37648c0d77678b107186f127b
PAlagusurya <alagusuryap@gmail.com>
2025-01-26 20:56:02 +0530

    for-in

---
+++ JS_Concepts/for/for-in.js
@@ -0,0 +1,23 @@
+const person = {
+  //   name: "John",
+  //   age: 30,
+  //   city: "New York",
+};
+
+for (let key in person) {
+  console.log(key, person[key]);
+}
+
+/**
+ * 
+
+- Do not use `for in` on arrays:
+  - It's not recommended for arrays because it may iterate over properties rather than values.
+  - Use array methods (e.g., `forEach`, `map`) or the `for of` loop for arrays.
+
+---
+
+- The `for in` loop is ideal for objects.
+- For arrays, prefer methods or `for of` loops.
+
+ */
